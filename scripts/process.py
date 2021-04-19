3effc5a1e2551dc73a958151b1f6b8a815b797b3
Dave Foster <dave@axiomdatascience.com>
2021-04-19 12:09:13 -0400

    Working polygon production for distribution

--- scripts/process.py
+++ scripts/process.py
@@ -1,5 +1,5 @@
 import json
-from functools import cache
+from functools import cache, singledispatch
 from pathlib import PosixPath as Path
 from typing import Callable, Dict, List, Optional, Union
 
@@ -9,9 +9,11 @@ import pandas as pd
 import geojson
 import geopandas
 import pyvisgraph as vg
-from shapely.geometry import asPolygon, box, LineString, MultiPoint, Point
+from shapely.geometry import asPolygon, box, LineString, MultiPoint, Point, Polygon, MultiPolygon
 from shapely.geometry.base import BaseGeometry
 from shapely_geojson import Feature, FeatureCollection
+from skimage.measure import find_contours
+from scipy.interpolate import interp1d
 
 # what is this?!?
 try:
@@ -120,7 +122,7 @@ def get_interp_line(lon_start: float, lat_start: float, lon_end: float, lat_end:
     raise ValueError(f"Unknown method ({method}) for get_interp_line")
 
 
-def animal_interpolated_paths(df: pd.DataFrame) -> geopandas.GeoDataFrame:
+def animal_interpolated_paths(df: pd.DataFrame, interp_method: str="simple") -> geopandas.GeoDataFrame:
     """
     For each animal, interpolate the path between detections (daily).
     """
@@ -165,7 +167,7 @@ def animal_interpolated_paths(df: pd.DataFrame) -> geopandas.GeoDataFrame:
                 continue
 
             # make a path between the begin and end
-            line = get_interp_line(begin.longitude, begin.latitude, end.longitude, end.latitude, method='visgraph')
+            line = get_interp_line(begin.longitude, begin.latitude, end.longitude, end.latitude, method=interp_method)
 
             # normalize that date range so we can feed data to the linestring interpolate
             # https://stackoverflow.com/a/41532180
@@ -227,22 +229,44 @@ def animal_interpolated_paths(df: pd.DataFrame) -> geopandas.GeoDataFrame:
 
             anim_gjos.append(gjo)
 
+        # CACHE ON DISK
+
         anim_fc = FeatureCollection(anim_gjos)
         fname = f"tmp/at/lines_vis_{name}.geojson"
         print("writing", fname)
         to_geojson(anim_fc, fname)
 
-        full_gdf.set_index(pd.Index([name] * len(full_gdf)), append=True, inplace=True)
-        animal_tracks.append(full_gdf)
+        # full_gdf.set_index(pd.Index([name] * len(full_gdf)), append=True, inplace=True)
+        feature_gdf = geopandas.GeoDataFrame.from_features(anim_gjos)
+        animal_tracks.append(feature_gdf)
 
         # animal_line = MultiPoint(geopandas.points_from_xy(full_gdf.longitude, full_gdf.latitude))
         # fname = f"tmp/at/{name}.geojson"
         # print("writing", fname)
         # to_geojson(animal_line, fname)
 
-    all_interp_animals = pd.concat(animal_tracks)
+    all_interp_animals = pd.concat(animal_tracks, ignore_index=True)
+    all_gdf = geopandas.GeoDataFrame(all_interp_animals, geometry='geometry', crs='EPSG:4326')
 
-    return geopandas.GeoDataFrame()
+    # make a grid, join them
+    matched_gdf = _match_with_grid(all_gdf)
+
+    # transform to points
+    matched_gdf.geometry = matched_gdf.geometry.apply(lambda x: x.centroid)
+
+    # build contour polygons
+    contour_polys = make_contour_polygons(matched_gdf, levels=list(range(1, int(matched_gdf['counts'].max()) + 1)))
+
+    # smooth polygons out
+    smoothed = [smooth_polygon(cpoly) for cpoly in contour_polys]
+
+    # save to disk
+    fc = FeatureCollection(smoothed)
+    fname = f"tmp/at/final-contours-smoothed.geojson"
+    print("writing", fname)
+    to_geojson(fc, fname)
+
+    return all_gdf
 
 
 @cache
@@ -272,6 +296,179 @@ def build_vis_graph(filename: str=None) -> vg.VisGraph:
 
     g.save('data/landvisgraph.pk1')
     return g
+
+
+# https://github.com/adaj/geohunter/blob/65c65a451f1edaa110de5629e4a3fa9c1cbaa50b/geohunter/util.py#L137-L169
+def make_gridsquares(city_shape, resolution=1):
+    """It constructs a grid of square cells.
+    Parameters
+    ----------
+    city_shape : GeoDataFrame.
+        Corresponds to the boundary geometry in which the grid will be formed.
+    resolution : float, default is 1.
+        Space between the square cells.
+    """
+    x0 = city_shape.bounds.min().values[0]
+    xf = city_shape.bounds.max().values[2]
+    y0 = city_shape.bounds.min().values[1]
+    yf = city_shape.bounds.max().values[3]
+    n_y = int((yf-y0)/(resolution/110.57))
+    n_x = int((xf-x0)/(resolution/111.32))
+    grid = {}
+    c = 0
+    for i in range(n_x):
+        for j in range(n_y):
+            grid[c] = {'geometry': Polygon([[x0, y0],
+                                            [x0+(resolution/111.32), y0],
+                                            [x0+(resolution/111.32),
+                                             y0+(resolution/110.57)],
+                                            [x0, y0+(resolution/110.57)]])}
+            c += 1
+            y0 += resolution/110.57
+        y0 = city_shape.bounds.min().values[1]
+        x0 += resolution/111.32
+    grid = pd.DataFrame(grid).transpose()
+    grid = geopandas.GeoDataFrame(grid, geometry='geometry',
+                            crs='EPSG:4326')
+    return grid
+    #grid = geopandas.sjoin(grid, city_shape, op='intersects')
+    #return grid[~grid.index.duplicated()]
+
+
+def _match_with_grid(animal_gdf: geopandas.GeoDataFrame, resolution: int=10) -> geopandas.GeoDataFrame:
+    """
+    @param 
+    # gdf is geopandas dataframe wrapped around shapely geometry for each animal,
+    """
+    # s_grid is secoora water bounds made into a grid by make_gridsquares
+    # really only need bounds though, we need the full square even over land, so likely don't need the water polygon
+    gdf = geopandas.read_file("data/secoora-water.geojson")
+    s_grid = make_gridsquares(gdf, resolution=resolution)
+
+    # join the grid squares with the animal geometry
+    match_counts = geopandas.sjoin(animal_gdf, s_grid).groupby('index_right').size().to_frame('counts')
+    joined = geopandas.GeoDataFrame(match_counts.join(s_grid, how='outer').fillna(0))
+
+    return joined
+
+    # alls = [] 
+    # for gdf in gdfs:
+        # alls.append(geopandas.sjoin(gdf, gsgdf).groupby('index_right').size().to_frame(gdf.fieldnumber[0]))
+
+    # join all the matched animal grids together and merge animal counts into them
+    # alldf = pd.concat(alls, axis=1).join(gsgdf, how='outer')
+
+    # add a count of all unique animals
+    # @TODO: may capture the same animal twice?
+    # alldf['count'] = alldf.sum(axis=1, numeric_only=True)
+
+    # to geodataframe
+    # return geopandas.GeoDataFrame(alldf, crs=gdfs[0].crs)
+
+
+def make_contour_polygons(gdf: geopandas.GeoDataFrame, levels: List[Union[int, float]]) -> List[Feature]:
+    """
+    Creates a contour filled polygon Feature per level.
+
+    @param gdf  GeoDataFrame of a grid of points, ordered by longitude major, latitude minor.
+    """
+    # figure out how many latitudes exist in the first column of longitudes
+    lon_diffs = gdf.geometry.map(lambda p: p.x).diff()
+    sig_diffs = lon_diffs[lon_diffs > 0.01]     # @TODO: prolly not good
+    assert len(sig_diffs)
+
+    num_lats = sig_diffs.index[0]
+    num_lons = len(gdf) // num_lats
+    assert len(gdf) % num_lats == 0       # make sure it was an even divisor
+
+    # extract lats and lons from dataframe
+    lats = gdf.geometry[0:num_lats].apply(lambda p: p.y).tolist()
+    lons = gdf.geometry[::num_lats].apply(lambda p: p.x).tolist()
+
+    # set up interpolators to convert from image space back into lat/lon space
+    interp_x = interp1d(np.arange(0, num_lons), lons)
+    interp_y = interp1d(np.arange(0, num_lats), lats)
+
+    # now extract count data and massage it into a 2d array
+    split_counts = np.split(gdf['counts'].to_numpy(), num_lons)
+    bits = np.stack(split_counts).transpose()       # go from lon major to lat major. makes far more sense in my head
+
+    # calculate all contours
+    contour_levels = []
+    for level in levels:
+        # calculate contours for the given level
+        # adjust level slightly because this method can only go > not >=
+        contours = find_contours(bits, level - 0.1)
+
+        # each contour in the list is an array of [lat, lon] indices (well, between indicies) that a contour line goes
+        # they form polygons, except near the edges, they might not be closed, but this should hopefully not be a problem
+        # with our giant area. famous last words.
+
+        polys = [
+            Polygon([
+                (interp_x(ix), interp_y(iy)) for iy, ix in contour
+            ]) for contour in contours
+        ]
+
+        fpoly = polys[0] if len(polys) == 1 else MultiPolygon(polys)
+
+        contour_levels.append(
+            Feature(
+                fpoly,
+                properties={
+                    'level': level
+                }
+            )
+        )
+
+    return contour_levels
+
+
+@singledispatch
+def smooth_polygon(poly: Polygon, refinements: int=5) -> Polygon:
+    assert len(poly.interiors) == 0, "Can't do interior rings yet"  # @TODO interior rings
+
+    return Polygon(
+        chaikins_corner_cutting(poly.exterior.coords, refinements=refinements)
+    )
+
+
+@smooth_polygon.register
+def _(poly: Feature, refinements: int=5) -> Feature:
+    return Feature(
+        smooth_polygon(poly.geometry, refinements=refinements),
+        properties=poly.properties
+    )
+
+
+@smooth_polygon.register
+def __(poly: MultiPolygon, refinements: int=5) -> MultiPolygon:
+    return MultiPolygon(
+        [smooth_polygon(p) for p in poly.geoms]
+    )
+
+
+def chaikins_corner_cutting(coords: Union[list, np.ndarray], refinements: int=5) -> np.ndarray:
+    """
+    Implements chaikin's algorithm to smooth polygons.
+    http://graphics.cs.ucdavis.edu/education/CAGDNotes/Chaikins-Algorithm/Chaikins-Algorithm.html
+
+    Implementation from:
+    https://stackoverflow.com/a/47255374/84732
+    """
+
+    coords = np.array(coords)
+
+    for _ in range(refinements):
+        L = coords.repeat(2, axis=0)
+        R = np.empty_like(L)
+        R[0] = L[0]
+        R[2::2] = L[1:-1:2]
+        R[1:-1:2] = L[2::2]
+        R[-1] = L[-1]
+        coords = L * 0.75 + R * 0.25
+
+    return coords
 
 
 agg_methods: Dict[str, Dict[str, Union[Callable, str]]] = {
