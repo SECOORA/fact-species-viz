351c15c309253c2706181af77ea339140c0e08f2
Dave Foster <dave@axiomdatascience.com>
2021-06-15 19:00:48 -0400

    Move to async processing, have single route to queue all default work

--- scripts/serve.py
+++ scripts/serve.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from . import tasks
-from .cache import get_projects_for_species, read_cache, get_species_ids, get_species_months, get_species_years, get_data_inventory
+from .cache import get_projects_for_species, read_cache, get_species_ids, get_species_months, get_species_years, get_data_inventory, get_citations
 from .log import logger
 from .utils import ATPType, get_atp_cache_key
 
@@ -30,10 +30,44 @@ async def process_atp_project(project_code: str, year: int, type: ATPType):
     # if month:
     #     kwargs['month'] = month
 
-    res = tasks.run_atp_process.apply(kwargs=kwargs)
-    ret_val = res.get()
+    res = tasks.run_atp_process.apply_async(kwargs=kwargs)
+    # res = tasks.run_atp_process.apply(kwargs=kwargs)
+    # ret_val = res.get()
 
-    return ret_val
+    return {'status': 'processing', 'args': kwargs}
+
+
+@app.post('/atp/PROCESS_DEFAULT')
+async def process_defaults():
+    projects = [
+        'BLKTP',
+        'FSUGG',
+        'FLKEYST',
+        'TQCS'
+    ]
+
+    years = list(range(2009, 2021))
+
+    types = [
+        ATPType.range,
+        ATPType.distribution
+    ]
+
+    ret = []
+
+    for p in projects:
+        for y in years:
+            for t in types:
+                kwargs = {
+                    'project_code': p,
+                    'year': y,
+                    'type': t.value
+                }
+
+                tasks.run_atp_process.apply_async(kwargs=kwargs)
+                ret.append(kwargs)
+
+    return {'status': 'processing', 'count': len(ret), 'args': ret}
 
 
 # @app.get('/atp/species')
